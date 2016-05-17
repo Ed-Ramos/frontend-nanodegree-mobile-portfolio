3769ffced57e4610dd25a4e55055d61f6da8f66b
Edwin ramos <Edwin ramos>
2016-05-16 21:44:19 -0400

    ready for submission

--- views/js/mainOrig.js
+++ views/js/mainOrig.js
@@ -449,19 +449,11 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
-     function changePizzaSizes(size) {
-
-        var randomPizzas = document.getElementsByClassName("randomPizzaContainer");
-        var randomPizzaLength = randomPizzas.length;
-        if (randomPizzaLength > 0){
-
-          var dx = determineDx(randomPizzas[0], size);
-          var newwidth = (randomPizzas[0].offsetWidth + dx) + 'px';
-
-          for (var i = 0; i < randomPizzaLength; i++) {
-          randomPizzas[i].style.width = newwidth;
-
-        }
+  function changePizzaSizes(size) {
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
@@ -510,14 +502,10 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var phase = [];
-  var X = (document.body.scrollTop / 1250);
-  for (var i = 0; i < 5; i++) {
-    phase.push(Math.sin(X + i));
-  }
-  var items = document.getElementsByClassName('mover');
+  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    items[i].style.left = items[i].basicLeft + 100 * phase[(i % 5)] + 'px';
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -537,7 +525,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 40; i++) {
+  for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
